a34071c942655832c0a03bb7d0cd547b764dce74
Arthur Mauricio <arthur.mauricio@academico.ifpb.edu.br>
2020-07-31 11:17:08 -0300

    feat: 🎸 init backend

---
+++ backend/src/controllers/RefrigeratorController.ts
@@ -0,0 +1,12 @@
+interface IndexConfig {
+  page?: Number;
+  max?: Number;
+}
+
+class ListController {
+  index({ page, max } : IndexConfig) {
+
+  }
+}
+
+export default new ListController();
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,5 +1,15 @@
 import express from 'express';
+import cors from 'cors';
+
+import database from '../database/frostFree.json';
 
 const app = express();
 
-app.listen(process.env.PORT || 3333);
+app.use(express.json());
+
+app.use(cors());
+
+app.listen(process.env.PORT || 3333, () => {
+  console.log(database.length);
+  console.log('Server on!');
+});
